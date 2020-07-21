4d1baa6e7c9ece14d3c643f322f8d4ba36d08623
Ross Williams <rossjrw@gmail.com>
2020-07-21 03:58:30 +0100

    Setup everything

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+};
