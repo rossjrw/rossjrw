b92f0ec6240c0f0f7ee90bef7784aa6824be337e
Ross Williams <rossjrw@gmail.com>
2020-07-21 12:59:35 +0100

    Add error handling

--- src/play.ts
+++ src/play.ts
@@ -1,7 +1,12 @@
 import { Octokit } from "@octokit/rest"
 import { Context } from "@actions/github/lib/context"
 
+interface ErrorDescriptions {
+  [error_type: string]: string
+}
+
 export function play (
+  title: string,
   octokit: Octokit,
   context: Context,
 ): void {
@@ -11,6 +16,7 @@ export function play (
    * This project is inspired by timburgan/timburgan - thank you Tim!
    *
    * Recieves parameters from actions/github-script:
+   * @param title: The title of the triggering issue.
    * @param octokit: octokit/rest.js client.
    * @param context: Workflow context object.
    * @returns void: At least until I work out what this should do!
@@ -21,10 +27,96 @@ export function play (
   // First thing to do: add a reaction to the triggering commit to acknowledge
   // that we've seen it.
   // TODO React with a rocket once the issue has been actioned.
+  addReaction("eyes", octokit, context)
+
+  // Parse the issue's title into a concrete action
+  try {
+    const [command, move, gameId] = parseIssueTitle(title)
+  } catch (error) {
+    // If there was an error, forward it to the user, then stop
+    handleError(error, octokit, context)
+    return
+  }
+}
+
+function handleError(
+  error: Error,
+  octokit: Octokit,
+  context: Context,
+): void {
+  /**
+   * Handles execution errors by reporting the problem back to the user and
+   * closing the issue.
+   *
+   * @param error: The error to report with an ID matching the desc object.
+   */
+  const ERROR_DESC: ErrorDescriptions = {
+    WRONG_GAME: "Sorry, I only know how to play Ur.",
+    UNKNOWN_COMMAND: "I'm not sure what you're asking me to do - the only commands I know are 'new' and 'move'.",
+    NO_MOVE_GIVEN: "You've asked me to make a move, but you haven't told me which move to make.",
+    NO_ID_GIVEN: "You've told me what move to make, but I'm not sure where I should make it without a game ID.",
+    MOVE_BAD_FORMAT: "You've asked me to make a move, but I'm not sure what exactly you want me to do. Is your move in the right format?",
+    NON_NUMERIC_ID: "You've told me what move to make, but the game ID you've given me isn't a number.",
+  }
+  addReaction("confused", octokit, context)
+  octokit.issues.createComment({
+    owner: context.issue.owner,
+    repo: context.issue.repo,
+    issue_number: context.issue.number,
+    body: ERROR_DESC[error.message],
+  })
+  octokit.issues.update({
+    owner: context.issue.owner,
+    repo: context.issue.repo,
+    issue_number: context.issue.number,
+    state: "closed",
+  })
+}
+
+function addReaction (
+  reaction: NonNullable<Parameters<typeof octokit.reactions.createForIssue>[0]>["content"],
+  octokit: Octokit,
+  context: Context,
+): void {
+  /**
+   * Adds a reaction to the triggering issue.
+   */
   octokit.reactions.createForIssue({
     owner: context.issue.owner,
     repo: context.issue.repo,
     issue_number: context.issue.number,
-    content: "eyes",
+    content: reaction,
   })
+}
+
+function parseIssueTitle (
+  title: string,
+): ["new" | "move", string, number] {
+  /**
+   * Parses the issue title into move instructions.
+   *
+   * @param title: The title of the issue.
+   */
+  const [gamename, command, move, gameId] = title.split("-")
+  if (!gamename || gamename !== "ur") {
+    throw new Error('WRONG_GAME')
+  }
+  if (!command || !["new", "move"].includes(command)) {
+    throw new Error('UNKNOWN_COMMAND')
+  }
+  if (command === "move") {
+    if (!move) {
+      throw new Error('NO_MOVE_GIVEN')
+    }
+    if (!gameId) {
+      throw new Error('NO_ID_GIVEN')
+    }
+    if (!/\d+@\d+/.test(move)) {
+      throw new Error('MOVE_BAD_FORMAT')
+    }
+    if(isNaN(parseInt(gameId))) {
+      throw new Error('NON_NUMERIC_ID')
+    }
+  }
+  return [command as "new" | "move", move, parseInt(gameId)]
 }
