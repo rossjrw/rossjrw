f4172a1a576ff5e49778ef0b7ef3e302e19b42e9
Ross Williams <rossjrw@gmail.com>
2020-07-24 18:54:00 +0100

    Use commonjs2 export type

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    library: "play",
+    libraryTarget: "commonjs2",
   },
   module: {
     rules: [
