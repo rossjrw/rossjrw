8e9b81bf6e83655905d55d507afe9758e2b081ef
Ross Williams <rossjrw@gmail.com>
2020-07-28 18:39:33 +0100

    Make getState method more generic

--- src/getFile.ts
+++ src/getFile.ts
@@ -1,24 +1,23 @@
 import { Octokit } from "@octokit/rest/index"
 import { Context } from "@actions/github/lib/context"
 
-export async function getStateFile (
+export async function getFile (
+  filename: string,
   gamePath: string,
   octokit: Octokit,
   context: Context,
 ): Promise<Octokit.Response<Octokit.ReposGetContentsResponse> | null> {
   /**
-   * Gets the current game state as stored on file. If there is no state on
-   * file, returns null.
+   * Gets the content of a file, or returns null if it doesn't exist.
    */
 
   // Grab the content of the current board from file
-  let stateFile
   try {
-    stateFile = await octokit.repos.getContents({
+    return await octokit.repos.getContents({
       owner: context.issue.owner,
       repo: context.issue.repo,
       ref: "play",
-      path: `${gamePath}/state.json`,
+      path: `${gamePath}/${filename}`,
       mediaType: { format: "raw" },
     })
   } catch (error) {
@@ -29,5 +28,4 @@ export async function getStateFile (
       throw error
     }
   }
-  return stateFile
 }
