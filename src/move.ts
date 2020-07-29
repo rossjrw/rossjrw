709ba3513efc4fd81dee5ab5303e930d2538de46
Ross Williams <rossjrw@gmail.com>
2020-07-30 00:13:45 +0100

    Make log entry before README generation

--- src/move.ts
+++ src/move.ts
@@ -124,16 +124,16 @@ export async function makeMove (
     })
   }
 
-  // Update README.md with the new state
-  changes = changes.concat(
-    await generateReadme(newState, gamePath, octokit, context, log)
-  )
-
   // Update the log with this action
   log.addToLog(
     "move",
     `${events.ascensionHappened ? "ascended" : "moved"} a ${state.currentPlayer === Ur.BLACK ? "black" : "white"} piece ${fromPosition === 0 ? "onto the board" : `from position ${fromPosition}`} ${events.ascensionHappened ? ":rocket:" : `to position ${toPosition}${events.captureHappened ? ` — captured a ${state.currentPlayer === Ur.BLACK ? "white" : "black"} piece :crossed_swords:` : ""}. `}${events.rosetteClaimed ? " — claimed a rosette :rosette:" : ""}${events.gameWon ? " — won the game :crown:" : ""}`,
     state.currentPlayer,
+  )
+
+  // Update README.md with the new state
+  changes = changes.concat(
+    await generateReadme(newState, gamePath, octokit, context, log)
   )
 
   return changes
