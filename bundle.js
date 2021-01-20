aeb9e6ae54813148ae67a99c3df746f8bcf3ef71
rossjrw <rossjrw@gmail.com>
2021-01-20 16:32:35 +0000

    Deploying to compiled from  @ 2e6db5bef5c62520d905f8e0b71c4ba0f2d34989 🚀

--- bundle.js
+++ bundle.js
@@ -26993,14 +26993,14 @@ function listPreviousGames(gamePath, octokit, context) {
                         var lastMove = log[log.length - 1];
                         var playerCount = Object(lodash__WEBPACK_IMPORTED_MODULE_2__["uniq"])(log.map(function (entry) { return entry.username; })).length;
                         var mvp = Object(lodash__WEBPACK_IMPORTED_MODULE_2__["flow"])(lodash__WEBPACK_IMPORTED_MODULE_2__["countBy"], lodash__WEBPACK_IMPORTED_MODULE_2__["entries"], Object(lodash__WEBPACK_IMPORTED_MODULE_2__["partialRight"])(lodash__WEBPACK_IMPORTED_MODULE_2__["maxBy"], lodash__WEBPACK_IMPORTED_MODULE_2__["last"]), lodash__WEBPACK_IMPORTED_MODULE_2__["head"])(log.filter(function (logItem) { return logItem.team === lastMove.team; }).map(function (logItem) { return logItem.username; }));
-                        return Object(compress_tag__WEBPACK_IMPORTED_MODULE_1__["compress"])(templateObject_2 || (templateObject_2 = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__makeTemplateObject"])(["\n      A game was started\n      on ", "\n      by **[@", "](https://github.com/", ")**\n      and ended on ", "\n      in a win for the ", " team.\n      ", " players\n      played ", " moves\n      across ", ".\n      The :black_circle:black team captured ", " white pieces and claimed ", " rosettes.\n      The :white_circle:white team captured ", " black pieces and claimed ", " rosettes.\n      The MVP of the winning team was\n      by **[@", "](https://github.com/", ")**,\n      who played ", " moves.\n      The winning move was made\n      by **[@", "](https://github.com/", ")**\n      ([#", "](https://github.com/", "/", "/issues/", ")).\n    "], ["\n      A game was started\n      on ", "\n      by **[@", "](https://github.com/", ")**\n      and ended on ", "\n      in a win for the ",
-                            " team.\n      ", " players\n      played ", " moves\n      across ",
-                            ".\n      The :black_circle:black team captured ",
+                        return Object(compress_tag__WEBPACK_IMPORTED_MODULE_1__["compress"])(templateObject_2 || (templateObject_2 = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__makeTemplateObject"])(["\n      A game was started\n      on ", "\n      by **[@", "](https://github.com/", ")**\n      The game ended on ", ".\n      <> The ", " team won.\n      <> ", " players played ", " moves across ", ".\n      <> The :black_circle:black team captured ", " white pieces and claimed ", " rosettes.\n      <> The :white_circle:white team captured ", " black pieces and claimed ", " rosettes.\n      <> The MVP of the winning team was\n      **[@", "](https://github.com/", ")**,\n      who played ", " moves.\n      <> The winning move was made\n      by **[@", "](https://github.com/", ")**\n      ([#", "](https://github.com/", "/", "/issues/", ")).\n    "], ["\n      A game was started\n      on ", "\n      by **[@", "](https://github.com/", ")**\n      The game ended on ", ".\n      <> The ",
+                            " team won.\n      <> ", " players played ", " moves across ",
+                            ".\n      <> The :black_circle:black team captured ",
                             " white pieces and claimed ",
-                            " rosettes.\n      The :white_circle:white team captured ",
+                            " rosettes.\n      <> The :white_circle:white team captured ",
                             " black pieces and claimed ",
-                            " rosettes.\n      The MVP of the winning team was\n      by **[@", "](https://github.com/", ")**,\n      who played ",
-                            " moves.\n      The winning move was made\n      by **[@", "](https://github.com/", ")**\n      ([#", "](https://github.com/", "/", "/issues/", ")).\n    "])), dateformat__WEBPACK_IMPORTED_MODULE_4___default()(new Date(firstMove.time), "dS mmm yyyy"), firstMove.username, firstMove.username, dateformat__WEBPACK_IMPORTED_MODULE_4___default()(new Date(lastMove.time), "dS mmm yyyy"), lastMove.team === "b" ?
+                            " rosettes.\n      <> The MVP of the winning team was\n      **[@", "](https://github.com/", ")**,\n      who played ",
+                            " moves.\n      <> The winning move was made\n      by **[@", "](https://github.com/", ")**\n      ([#", "](https://github.com/", "/", "/issues/", ")).\n    "])), dateformat__WEBPACK_IMPORTED_MODULE_4___default()(new Date(firstMove.time), "dS mmm yyyy"), firstMove.username, firstMove.username, dateformat__WEBPACK_IMPORTED_MODULE_4___default()(new Date(lastMove.time), "dS mmm yyyy"), lastMove.team === "b" ?
                             ":black_circle:black" :
                             ":white_circle:white", playerCount, log.length, humanize_duration__WEBPACK_IMPORTED_MODULE_3___default()(new Date(lastMove.time).getTime() -
                             new Date(firstMove.time).getTime(), { largest: 2, delimiter: " and " }), log.filter(function (logItem) {
@@ -27015,7 +27015,7 @@ function listPreviousGames(gamePath, octokit, context) {
                         }).length, log.filter(function (logItem) {
                             var _a;
                             return logItem.team === "w" && ((_a = logItem.events) === null || _a === void 0 ? void 0 : _a.rosetteClaimed);
-                        }).length, mvp, mvp, log.filter(function (logItem) { return logItem.username === mvp; }).length, lastMove.username, lastMove.username, lastMove.issue, context.repo.owner, context.repo.repo, lastMove.issue);
+                        }).length, mvp, mvp, log.filter(function (logItem) { return logItem.username === mvp; }).length, lastMove.username, lastMove.username, lastMove.issue, context.repo.owner, context.repo.repo, lastMove.issue).replace(/<>/g, "\n  *");
                     });
                     return [2 /*return*/, gameStrings];
             }
